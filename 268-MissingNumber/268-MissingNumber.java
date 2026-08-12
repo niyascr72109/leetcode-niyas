ffb40af099cae17a12bc5b453aeebee736b8f556
Niyas Ahamed M <niyascr71011@gmail.com>
2026-08-12 22:14:54 +0530

    File updated at 8/12/2026, 10:14:54 PM

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,14 +1,20 @@
-// Last updated: 8/12/2026, 10:13:27 PM
-1class Solution {
-2    public int maxSubArray(int[] nums) {
-3        int n = nums.length;
-4        int sum = 0;
-5        int maxi = nums[0];
-6        for(int i=0;i<n;i++){
-7            sum+=nums[i];
-8            if(sum > maxi) maxi = sum;
-9            if(sum < 0) sum = 0;
-10        }
-11        return maxi;
-12    }
-13}
+// Last updated: 8/12/2026, 10:14:54 PM
+class Solution {
+    static {
+        for (int i = 0; i < 500; i++)
+            maxSubArray(new int[1]);
+    }
+    public static int maxSubArray(int[] nums) {
+       int n = nums.length;
+       if(n == 1) return nums[0];
+
+       int sum =0;
+       int max = nums[0];
+       for(int i = 0; i < n; i++) {
+        sum += nums[i];
+        max = Math.max(max, sum);
+        if(sum < 0) sum = 0;
+       }
+       return max;
+    }
+}
