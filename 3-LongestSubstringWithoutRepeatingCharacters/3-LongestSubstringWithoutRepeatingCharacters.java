c0a331a1aaf3dba2a2be0c5cc079f33c214a2a13
Niyas Ahamed M <niyascr71011@gmail.com>
2026-08-30 12:44:50 +0530

    File updated at 8/30/2026, 12:44:52 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,20 +1,21 @@
-// Last updated: 8/30/2026, 10:53:41 AM
+// Last updated: 8/30/2026, 12:44:52 PM
 1class Solution {
-2    public int lengthOfLongestSubstring(String s) {
-3        
-4        int l = 0;
-5        int r = 0;
-6        int arr[] = new int[256];
-7        Arrays.fill(arr,-1);
-8        int longest = 0;
-9        while(r<s.length()){
-10            if(arr[s.charAt(r)]!=-1){
-11                l=Math.max(l,arr[s.charAt(r)]+1);
-12            }
-13            longest=Math.max(longest,r-l+1);
-14            arr[s.charAt(r)] = r;
-15            r++;
-16        }
-17        return longest;
-18    }
-19}
+2    public int longestOnes(int[] nums, int k) {
+3        int l = 0;
+4        int r = 0;
+5        int zeros = 0;
+6        int max = 0;
+7        while(r<nums.length){
+8            if(nums[r]==0) zeros++;
+9            if(zeros>k){
+10                if(nums[l]==0){
+11                    zeros--;
+12                }
+13                l++;
+14            }
+15            max=Math.max(max,r-l+1);
+16            r++;
+17        }
+18        return max;
+19    }
+20}
