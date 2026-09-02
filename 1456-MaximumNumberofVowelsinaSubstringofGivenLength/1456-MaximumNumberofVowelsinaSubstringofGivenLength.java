7aca90595e9c412808c6e2c72095044a310449d2
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-02 21:18:07 +0530

    File updated at 9/2/2026, 9:18:07 PM

--- 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.java
+++ 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.java
@@ -1,19 +1,22 @@
-// Last updated: 9/2/2026, 9:02:33 PM
+// Last updated: 9/2/2026, 9:18:07 PM
 1class Solution {
-2    public int minSubArrayLen(int target, int[] nums) {
-3        int l = 0;
-4        int r = 0;
-5        int sum = 0;
-6        int min = Integer.MAX_VALUE;
+2    public int numSubarrayProductLessThanK(int[] nums, int k) {
+3        int mul = 1;
+4        int l = 0;
+5        int r = 0;
+6        int c = 0;
 7        while(r<nums.length){
-8            sum += nums[r];
-9            while(sum >= target){
-10                min=Math.min(min,r-l+1);
-11                sum-= nums[l];
-12                l++;
-13            }
-14            r++;
-15        }
-16        return min==Integer.MAX_VALUE ? 0 : min;
-17    }
-18}
+8            mul*=nums[r];
+9            while(mul > k){
+10                mul/=nums[l];
+11                l++;
+12            }
+13            if(mul < k){
+14                c+=r-l+1;
+15            }
+16            r++;
+17
+18        }
+19        return c;
+20    }
+21}
