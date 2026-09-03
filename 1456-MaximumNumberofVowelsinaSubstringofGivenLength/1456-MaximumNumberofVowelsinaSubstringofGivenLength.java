6d2b86f40ce353b1160f2c3da3102d8db3bf57bd
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-03 09:25:25 +0530

    File updated at 9/3/2026, 9:25:25 AM

--- 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.java
+++ 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.java
@@ -1,24 +1,23 @@
-// Last updated: 9/2/2026, 9:57:17 PM
+// Last updated: 9/3/2026, 9:25:25 AM
 1class Solution {
-2    public int longestOnes(int[] nums, int k) {
-3        int max = 0;
-4        int l = 0;
-5        int r = 0;
-6        int zero = 0;
-7        int c = 0;
-8        while(r<nums.length){
-9            if(nums[r]==0){
-10                zero++;
-11            }
-12            while(zero > k){
-13                if(nums[l]==0){
-14                zero--;
-15                }
-16                l++;
-17            }
-18            max=Math.max(max,r-l+1);
-19            r++;
-20        }
-21        return max;
-22    }
-23}
+2    public int totalFruit(int[] fruits) {
+3        int l = 0;
+4        int r = 0;
+5        int max = 0;
+6        int len = 0;
+7        HashMap<Integer,Integer> h = new HashMap<>();
+8        while(r<fruits.length){
+9            h.put(fruits[r],h.getOrDefault(fruits[r],0)+1);
+10            while(h.size()>2){
+11                h.put(fruits[l],h.getOrDefault(fruits[l],0)-1);
+12                if(h.get(fruits[l]) == 0){
+13                    h.remove(fruits[l]);
+14                }
+15                l++;
+16            }
+17            max=Math.max(max,r-l+1);
+18            r++;
+19        }
+20        return max;
+21    }
+22}
