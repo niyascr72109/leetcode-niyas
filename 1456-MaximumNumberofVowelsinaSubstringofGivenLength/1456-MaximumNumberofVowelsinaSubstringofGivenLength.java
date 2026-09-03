c41772b5d5146dfdc4a1f82d0831d34b6694de8f
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-03 12:03:33 +0530

    File updated at 9/3/2026, 12:03:33 PM

--- 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.java
+++ 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.java
@@ -1,26 +1,25 @@
-// Last updated: 9/3/2026, 11:37:32 AM
+// Last updated: 9/3/2026, 12:03:33 PM
 1class Solution {
 2    public int countKConstraintSubstrings(String s, int k) {
-3        int l = 0;
-4        int r = 0;
-5        int c = 0;
-6        int one = 0;
-7        int zero = 0;
-8        while(r<s.length()){
-9            if(s.charAt(r)=='0') zero++;
-10            if(s.charAt(r)=='1') one++;
-11
-12            while(zero > k && one > k){
-13                if(s.charAt(l) == '0'){
-14                    zero--;
-15                }
-16                else one--;
-17                l++;
-18            }
-19            c+=r-l+1;
-20            r++;
-21            
-22        }
-23        return c;
-24    }
-25}
+3        int co=0;
+4        int l=0;
+5        HashMap<Character,Integer> m=new HashMap<>();
+6        for(int i=0;i<s.length();i++){
+7            char c=s.charAt(i);
+8            if(m.containsKey(c))
+9            m.put(c,m.get(c)+1);
+10            else
+11            m.put(c,1);
+12            while(m.getOrDefault('0',0) >k && m.getOrDefault('1',0) > k){
+13                char le=s.charAt(l);
+14                m.put(le,m.get(le)-1);
+15                if(m.get(le)=='0'){
+16                    m.remove(le);
+17                }
+18                l++;
+19            }
+20            co+=i-l+1;
+21        }
+22        return co;
+23    }
+24}
