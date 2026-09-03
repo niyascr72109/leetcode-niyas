b53f4559131f120f215d2965caf96fb0bda38130
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-03 11:37:33 +0530

    File updated at 9/3/2026, 11:37:32 AM

--- 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.java
+++ 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.java
@@ -1,22 +1,26 @@
-// Last updated: 9/3/2026, 11:08:09 AM
+// Last updated: 9/3/2026, 11:37:32 AM
 1class Solution {
-2    public boolean containsNearbyDuplicate(int[] nums, int k) {
-3        HashSet<Integer> h = new HashSet<>();
-4        int l = 0;
-5        int r = 0;
-6        while(r<nums.length){
-7            if(h.contains(nums[r])){
-8                return true;
-9            }
-10
-11            h.add(nums[r]);
-12
-13            if(h.size() > k){
-14                h.remove(nums[r-k]);
-15                l++;
-16            }
-17            r++;
-18        }
-19        return false;
-20    }
-21}
+2    public int countKConstraintSubstrings(String s, int k) {
+3        int l = 0;
+4        int r = 0;
+5        int c = 0;
+6        int one = 0;
+7        int zero = 0;
+8        while(r<s.length()){
+9            if(s.charAt(r)=='0') zero++;
+10            if(s.charAt(r)=='1') one++;
+11
+12            while(zero > k && one > k){
+13                if(s.charAt(l) == '0'){
+14                    zero--;
+15                }
+16                else one--;
+17                l++;
+18            }
+19            c+=r-l+1;
+20            r++;
+21            
+22        }
+23        return c;
+24    }
+25}
