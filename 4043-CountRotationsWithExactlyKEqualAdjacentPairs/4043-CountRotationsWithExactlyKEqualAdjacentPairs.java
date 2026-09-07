14bf1443a0ac53a301853fafa5dea0b06db910f6
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-07 14:41:59 +0530

    Create 4043-CountRotationsWithExactlyKEqualAdjacentPairs

---
+++ 4043-CountRotationsWithExactlyKEqualAdjacentPairs/4043-CountRotationsWithExactlyKEqualAdjacentPairs.java
@@ -0,0 +1,22 @@
+// Last updated: 9/7/2026, 2:41:59 PM
+1class Solution {
+2    public int countRotations(String s, int k) {
+3        int n = s.length();
+4        int c=0;
+5        for(int i=0;i<=n-1;i++)
+6        {
+7            if(s.charAt(i) == s.charAt((i+1)%n)) //circular array.
+8            {
+9                c++; //equal only 1
+10            }
+11        }
+12        int x = n-c;  //unequal 3-1 = 2;
+13
+14        
+15        if(k == c-1)    return c;
+16        if(k == c)  return x;
+17
+18        return 0;
+19        
+20    }
+21}
