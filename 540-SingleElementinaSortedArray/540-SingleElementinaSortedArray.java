551562ae76bea4ac2c42c41fd7dd786a39a68048
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-08 10:27:47 +0530

    File updated at 9/8/2026, 10:27:46 AM

--- 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.java
+++ 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.java
@@ -1,25 +1,31 @@
-// Last updated: 9/8/2026, 10:26:32 AM
+// Last updated: 9/8/2026, 10:27:46 AM
 1class Solution {
-2    public int compareVersion(String version1, String version2) {
-3        String[] v1 = version1.split("\\.");
-4        String[] v2 = version2.split("\\.");
-5
-6        int n = Math.max(v1.length, v2.length);
-7
-8        for (int i = 0; i < n; i++) {
-9
-10            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
-11            int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+2    public int characterReplacement(String s, int k) {
+3        int[] freq = new int[26];
+4
+5        int left = 0;
+6        int maxFreq = 0;
+7        int maxLength = 0;
+8
+9        for (int right = 0; right < s.length(); right++) {
+10
+11            freq[s.charAt(right) - 'A']++;
 12
-13            if (num1 < num2) {
-14                return -1;
-15            }
-16
-17            if (num1 > num2) {
-18                return 1;
-19            }
-20        }
-21
-22        return 0;
-23    }
-24}
+13            maxFreq = Math.max(maxFreq,
+14                    freq[s.charAt(right) - 'A']);
+15
+16            int windowLength = right - left + 1;
+17
+18            int changes = windowLength - maxFreq;
+19
+20            if (changes > k) {
+21                freq[s.charAt(left) - 'A']--;
+22                left++;
+23            }
+24
+25            maxLength = Math.max(maxLength, right - left + 1);
+26        }
+27
+28        return maxLength;
+29    }
+30}
