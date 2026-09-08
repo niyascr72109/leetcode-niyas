086425b01eba9b504c85bbd74e32efa9851ac7b7
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-08 10:10:33 +0530

    File updated at 9/8/2026, 10:10:33 AM

--- 3870-CountCommasinRange/3870-CountCommasinRange.java
+++ 3870-CountCommasinRange/3870-CountCommasinRange.java
@@ -1,11 +1,9 @@
-// Last updated: 9/8/2026, 10:07:59 AM
+// Last updated: 9/8/2026, 10:10:33 AM
 1class Solution {
 2    public int countCommas(int n) {
-3        if(n > 999){
-4            return n-999;
+3        if(n <= 999){
+4            return 0;
 5        }
-6        else{
-7            return 0;
-8        }    
-9    }
-10}
+6        return n-999;  
+7    }
+8}
