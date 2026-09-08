4854d198bdcf1d59fa52bcfdd3c4c1e049fb92b2
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-08 10:22:50 +0530

    File updated at 9/8/2026, 10:22:49 AM

--- 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.java
+++ 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.java
@@ -1,29 +1,24 @@
-// Last updated: 9/8/2026, 10:21:39 AM
+// Last updated: 9/8/2026, 10:22:49 AM
 1class Solution {
-2    public int minEatingSpeed(int[] piles, int h) {
-3        int low = 1;
-4        int high = 0;
-5
-6        for (int pile : piles) {
-7            high = Math.max(high, pile);
-8        }
-9
-10        while (low < high) {
-11            int mid = low + (high - low) / 2;
-12
-13            long hours = 0;
-14
-15            for (int pile : piles) {
-16                hours += (pile + mid - 1) / mid;
-17            }
-18
-19            if (hours <= h) {
-20                high = mid;
-21            } else {
-22                low = mid + 1;
-23            }
-24        }
-25
-26        return low;
-27    }
-28}
+2    public int maxArea(int[] height) {
+3        int left = 0;
+4        int right = height.length - 1;
+5        int max = 0;
+6
+7        while (left < right) {
+8            int width = right - left;
+9            int h = Math.min(height[left], height[right]);
+10
+11            int area = width * h;
+12            max = Math.max(max, area);
+13
+14            if (height[left] < height[right]) {
+15                left++;
+16            } else {
+17                right--;
+18            }
+19        }
+20
+21        return max;
+22    }
+23}
