3f99cc74ba6f61fb4d71a1b925cacddc0c718fb5
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-08 10:28:58 +0530

    File updated at 9/8/2026, 10:28:58 AM

--- 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.java
+++ 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.java
@@ -1,31 +1,36 @@
-// Last updated: 9/8/2026, 10:27:46 AM
-1class Solution {
-2    public int characterReplacement(String s, int k) {
-3        int[] freq = new int[26];
-4
-5        int left = 0;
-6        int maxFreq = 0;
-7        int maxLength = 0;
-8
-9        for (int right = 0; right < s.length(); right++) {
-10
-11            freq[s.charAt(right) - 'A']++;
-12
-13            maxFreq = Math.max(maxFreq,
-14                    freq[s.charAt(right) - 'A']);
-15
-16            int windowLength = right - left + 1;
-17
-18            int changes = windowLength - maxFreq;
+// Last updated: 9/8/2026, 10:28:58 AM
+1import java.util.*;
+2
+3class Solution {
+4    public List<Integer> findAnagrams(String s, String p) {
+5        List<Integer> result = new ArrayList<>();
+6
+7        int[] pFreq = new int[26];
+8        int[] windowFreq = new int[26];
+9
+10        int k = p.length();
+11
+12        // Frequency of p
+13        for (char c : p.toCharArray()) {
+14            pFreq[c - 'a']++;
+15        }
+16
+17        // Sliding window
+18        for (int right = 0; right < s.length(); right++) {
 19
-20            if (changes > k) {
-21                freq[s.charAt(left) - 'A']--;
-22                left++;
-23            }
-24
-25            maxLength = Math.max(maxLength, right - left + 1);
-26        }
-27
-28        return maxLength;
-29    }
-30}
+20            windowFreq[s.charAt(right) - 'a']++;
+21
+22            // Keep window size equal to p.length()
+23            if (right >= k) {
+24                windowFreq[s.charAt(right - k) - 'a']--;
+25            }
+26
+27            // Compare frequencies
+28            if (Arrays.equals(pFreq, windowFreq)) {
+29                result.add(right - k + 1);
+30            }
+31        }
+32
+33        return result;
+34    }
+35}
