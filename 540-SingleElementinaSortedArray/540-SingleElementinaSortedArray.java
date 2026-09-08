985673d21a90a57b61f71cd0b1d5251272e6d47a
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-08 10:30:43 +0530

    File updated at 9/8/2026, 10:30:41 AM

--- 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.java
+++ 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.java
@@ -1,36 +1,22 @@
-// Last updated: 9/8/2026, 10:28:58 AM
-1import java.util.*;
-2
-3class Solution {
-4    public List<Integer> findAnagrams(String s, String p) {
-5        List<Integer> result = new ArrayList<>();
+// Last updated: 9/8/2026, 10:30:41 AM
+1class Solution {
+2    public int minSubArrayLen(int target, int[] nums) {
+3        int left = 0;
+4        int sum = 0;
+5        int minLength = Integer.MAX_VALUE;
 6
-7        int[] pFreq = new int[26];
-8        int[] windowFreq = new int[26];
-9
-10        int k = p.length();
-11
-12        // Frequency of p
-13        for (char c : p.toCharArray()) {
-14            pFreq[c - 'a']++;
-15        }
-16
-17        // Sliding window
-18        for (int right = 0; right < s.length(); right++) {
-19
-20            windowFreq[s.charAt(right) - 'a']++;
-21
-22            // Keep window size equal to p.length()
-23            if (right >= k) {
-24                windowFreq[s.charAt(right - k) - 'a']--;
-25            }
-26
-27            // Compare frequencies
-28            if (Arrays.equals(pFreq, windowFreq)) {
-29                result.add(right - k + 1);
-30            }
-31        }
-32
-33        return result;
-34    }
-35}
+7        for (int right = 0; right < nums.length; right++) {
+8
+9            sum += nums[right];
+10
+11            while (sum >= target) {
+12                minLength = Math.min(minLength, right - left + 1);
+13
+14                sum -= nums[left];
+15                left++;
+16            }
+17        }
+18
+19        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+20    }
+21}
