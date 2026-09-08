68f64035f240a846afde42caddd24a99b6a51915
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-08 10:24:49 +0530

    File updated at 9/8/2026, 10:24:48 AM

--- 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.java
+++ 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.java
@@ -1,54 +1,23 @@
-// Last updated: 9/8/2026, 10:23:48 AM
-1import java.util.*;
-2
-3class Solution {
-4    public List<List<Integer>> threeSum(int[] nums) {
-5        List<List<Integer>> result = new ArrayList<>();
-6
-7        Arrays.sort(nums);
+// Last updated: 9/8/2026, 10:24:48 AM
+1class Solution {
+2    public int[] twoSum(int[] numbers, int target) {
+3        int left = 0;
+4        int right = numbers.length - 1;
+5
+6        while (left < right) {
+7            int sum = numbers[left] + numbers[right];
 8
-9        for (int i = 0; i < nums.length - 2; i++) {
-10
-11            // Skip duplicate first numbers
-12            if (i > 0 && nums[i] == nums[i - 1]) {
-13                continue;
+9            if (sum == target) {
+10                return new int[]{left + 1, right + 1};
+11            }
+12            else if (sum < target) {
+13                left++;
 14            }
-15
-16            int left = i + 1;
-17            int right = nums.length - 1;
-18
-19            while (left < right) {
-20
-21                int sum = nums[i] + nums[left] + nums[right];
-22
-23                if (sum == 0) {
-24                    result.add(Arrays.asList(
-25                        nums[i],
-26                        nums[left],
-27                        nums[right]
-28                    ));
-29
-30                    // Skip duplicates
-31                    while (left < right && nums[left] == nums[left + 1]) {
-32                        left++;
-33                    }
-34
-35                    while (left < right && nums[right] == nums[right - 1]) {
-36                        right--;
-37                    }
-38
-39                    left++;
-40                    right--;
-41
-42                } else if (sum < 0) {
-43                    left++;
-44
-45                } else {
-46                    right--;
-47                }
-48            }
-49        }
-50
-51        return result;
-52    }
-53}
+15            else {
+16                right--;
+17            }
+18        }
+19
+20        return new int[]{-1, -1};
+21    }
+22}
