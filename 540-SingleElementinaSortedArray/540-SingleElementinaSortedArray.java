101d4803d5380ea74605b5a5460a9194c2515a2d
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-08 10:21:40 +0530

    File updated at 9/8/2026, 10:21:39 AM

--- 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.java
+++ 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.java
@@ -1,26 +1,29 @@
-// Last updated: 9/8/2026, 10:20:41 AM
+// Last updated: 9/8/2026, 10:21:39 AM
 1class Solution {
-2    public int singleNonDuplicate(int[] nums) {
-3        int low = 0;
-4        int high = nums.length - 1;
+2    public int minEatingSpeed(int[] piles, int h) {
+3        int low = 1;
+4        int high = 0;
 5
-6        while (low < high) {
-7            int mid = low + (high - low) / 2;
-8
-9            // Make mid even
-10            if (mid % 2 == 1) {
-11                mid--;
-12            }
-13
-14            // Pair is correct: nums[mid] == nums[mid + 1]
-15            if (nums[mid] == nums[mid + 1]) {
-16                low = mid + 2;
-17            } 
-18            else {
-19                high = mid;
-20            }
-21        }
-22
-23        return nums[low];
-24    }
-25}
+6        for (int pile : piles) {
+7            high = Math.max(high, pile);
+8        }
+9
+10        while (low < high) {
+11            int mid = low + (high - low) / 2;
+12
+13            long hours = 0;
+14
+15            for (int pile : piles) {
+16                hours += (pile + mid - 1) / mid;
+17            }
+18
+19            if (hours <= h) {
+20                high = mid;
+21            } else {
+22                low = mid + 1;
+23            }
+24        }
+25
+26        return low;
+27    }
+28}
