8121bf2c5f555dd72e054d5f1b5669e5735bb2ae
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-08 10:26:33 +0530

    File updated at 9/8/2026, 10:26:32 AM

--- 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.java
+++ 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.java
@@ -1,15 +1,25 @@
-// Last updated: 9/8/2026, 10:25:33 AM
+// Last updated: 9/8/2026, 10:26:32 AM
 1class Solution {
-2    public int removeDuplicates(int[] nums) {
-3        int k = 0;
-4
-5        for (int num : nums) {
-6            if (k < 2 || num != nums[k - 2]) {
-7                nums[k] = num;
-8                k++;
-9            }
-10        }
-11
-12        return k;
-13    }
-14}
+2    public int compareVersion(String version1, String version2) {
+3        String[] v1 = version1.split("\\.");
+4        String[] v2 = version2.split("\\.");
+5
+6        int n = Math.max(v1.length, v2.length);
+7
+8        for (int i = 0; i < n; i++) {
+9
+10            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+11            int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+12
+13            if (num1 < num2) {
+14                return -1;
+15            }
+16
+17            if (num1 > num2) {
+18                return 1;
+19            }
+20        }
+21
+22        return 0;
+23    }
+24}
