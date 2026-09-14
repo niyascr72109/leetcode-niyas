f5584b6303043917cc102f75cc9df7ce8ef8b988
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-14 18:59:47 +0530

    File updated at 9/14/2026, 6:59:47 PM

--- 503-NextGreaterElementII/503-NextGreaterElementII.java
+++ 503-NextGreaterElementII/503-NextGreaterElementII.java
@@ -1,18 +1,27 @@
-// Last updated: 9/14/2026, 6:01:47 PM
+// Last updated: 9/14/2026, 6:59:47 PM
 1class Solution {
-2    public int[] nextGreaterElements(int[] nums) {
-3        int n = nums.length;
-4        int nge[] = new int[n];
-5        Stack<Integer> st = new Stack<>();
-6        for(int i=2*n-1;i>=0;i--){
-7            while(!st.isEmpty() && nums[i%n] >= st.peek()){
-8                st.pop();
-9            }
-10            if(i<n){
-11                nge[i] = st.isEmpty() ? -1 : st.peek();
-12            }
-13            st.push(nums[i%n]);
-14        }
-15        return nge;
-16    }
-17}
+2    public int trap(int[] height) {
+3        int lmax = 0;
+4        int rmax = 0;
+5        int t = 0;
+6        int l = 0;
+7        int r = height.length-1;
+8        while(l<r){
+9            if(height[l] <= height[r]){
+10                if(height[l] < lmax){
+11                    t+=lmax - height[l];
+12                }
+13                else lmax = height[l];
+14                l++;
+15            }
+16            else{
+17                if(height[r] < rmax){
+18                    t+=rmax - height[r];
+19                }
+20                else rmax = height[r];
+21                r--;
+22            }
+23        }
+24        return t;
+25    }
+26}
