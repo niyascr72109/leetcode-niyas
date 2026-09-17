0d247c9f5d5f90cd2ef5ae44e03b8eb9c0677038
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-17 19:45:42 +0530

    File updated at 9/17/2026, 7:45:42 PM

--- 682-BaseballGame/682-BaseballGame.java
+++ 682-BaseballGame/682-BaseballGame.java
@@ -1,32 +1,32 @@
-// Last updated: 9/17/2026, 7:18:58 PM
+// Last updated: 9/17/2026, 7:45:42 PM
 1class Solution {
-2    public int calPoints(String[] operations) {
-3        Stack<Integer> st = new Stack<>();
-4        for (int i = 0; i < operations.length; i++) {
-5            if (operations[i].equals("C")) {
-6                st.pop();
-7            }
-8            else if (operations[i].equals("D")) {
-9                int x = st.peek() * 2;
-10                st.push(x);
-11            }
-12            else if (operations[i].equals("+")) {
-13                int a = st.pop();
-14                int b = st.peek();
-15                st.push(a);
-16                st.push(a + b);
-17            }
-18            else {
-19                st.push(Integer.parseInt(operations[i]));
-20            }
-21        }
-22
-23        int ans = 0;
-24
-25        while (!st.isEmpty()) {
-26            ans += st.pop();
+2    public boolean backspaceCompare(String s, String t) {
+3
+4        Stack<Character> st = new Stack<>();
+5        Stack<Character> tt = new Stack<>();
+6
+7        for(char ch : s.toCharArray()) {
+8            if(ch == '#') {
+9                if(!st.isEmpty()) {
+10                    st.pop();
+11                }
+12            }
+13            else {
+14                st.push(ch);
+15            }
+16        }
+17
+18        for(char ch : t.toCharArray()) {
+19            if(ch == '#') {
+20                if(!tt.isEmpty()) {
+21                    tt.pop();
+22                }
+23            }
+24            else {
+25                tt.push(ch);
+26            }
 27        }
 28
-29        return ans;
+29        return st.equals(tt);
 30    }
 31}
