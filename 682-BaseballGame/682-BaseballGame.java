eafb42d1f59ee2d7377ad2a33f873f3476ee8d77
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-17 22:17:23 +0530

    File updated at 9/17/2026, 10:17:22 PM

--- 682-BaseballGame/682-BaseballGame.java
+++ 682-BaseballGame/682-BaseballGame.java
@@ -1,32 +1,27 @@
-// Last updated: 9/17/2026, 7:45:42 PM
+// Last updated: 9/17/2026, 10:17:22 PM
 1class Solution {
-2    public boolean backspaceCompare(String s, String t) {
-3
-4        Stack<Character> st = new Stack<>();
-5        Stack<Character> tt = new Stack<>();
-6
-7        for(char ch : s.toCharArray()) {
-8            if(ch == '#') {
-9                if(!st.isEmpty()) {
-10                    st.pop();
-11                }
-12            }
-13            else {
-14                st.push(ch);
+2    public int[] asteroidCollision(int[] asteroids) {
+3        Stack<Integer> st = new Stack<>();
+4        for(int i=0;i<asteroids.length;i++){
+5            boolean destroid = false;
+6            while(!st.isEmpty() && asteroids[i] < 0 && st.peek() > 0 && st.peek() + asteroids[i] <= 0){
+7                if(asteroids[i] + st.peek() < 0){
+8                    st.pop();
+9                }
+10                else{
+11                    st.pop();
+12                    destroid = true;
+13                    break;
+14                }
 15            }
-16        }
-17
-18        for(char ch : t.toCharArray()) {
-19            if(ch == '#') {
-20                if(!tt.isEmpty()) {
-21                    tt.pop();
-22                }
-23            }
-24            else {
-25                tt.push(ch);
-26            }
-27        }
-28
-29        return st.equals(tt);
-30    }
-31}
+16            if(!destroid){
+17                if(st.isEmpty() || asteroids[i] >0 || st.peek() < 0) st.push(asteroids[i]);
+18            }
+19        }
+20        int []res = new int[st.size()];
+21        for(int i=0;i<st.size() ; i++){
+22            res[i] = st.get(i);
+23        }
+24        return res;
+25    }
+26}
