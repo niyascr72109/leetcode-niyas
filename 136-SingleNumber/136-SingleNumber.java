82528218c8a3ba78a2952c90547cf6a85c579741
Niyas Ahamed M <niyascr71011@gmail.com>
2026-09-18 22:38:49 +0530

    File updated at 9/18/2026, 10:38:50 PM

--- 136-SingleNumber/136-SingleNumber.java
+++ 136-SingleNumber/136-SingleNumber.java
@@ -1,18 +1,34 @@
-// Last updated: 9/18/2026, 10:20:09 PM
+// Last updated: 9/18/2026, 10:38:50 PM
 1class Solution {
-2    public boolean canConstruct(String ransomNote, String magazine) {
-3        HashMap<Character,Integer> h = new HashMap<>();
-4        for(char ch : ransomNote.toCharArray()){
-5            h.put(ch,h.getOrDefault(ch,0)+1);
-6        }
-7        for(char ch : magazine.toCharArray()){
-8            if(h.containsKey(ch)){
-9                h.put(ch,h.get(ch)-1);
-10                if(h.get(ch) == 0){
-11                    h.remove(ch);
-12                }
-13            }
-14        }
-15        return h.isEmpty();
-16    }
-17}
+2    static boolean isVowel(char ch) {
+3        return ch == 'A' || ch == 'a' ||
+4               ch == 'E' || ch == 'e' ||
+5               ch == 'I' || ch == 'i' ||
+6               ch == 'O' || ch == 'o' ||
+7               ch == 'U' || ch == 'u';
+8    }
+9
+10    public String reverseVowels(String s) {
+11        char[] ch = s.toCharArray();
+12        int l = 0;
+13        int r = s.length() - 1;
+14        while (l < r) {
+15            if (!isVowel(ch[l])) {
+16                l++;
+17            }
+18            else if (!isVowel(ch[r])) {
+19                r--;
+20            }
+21            else {
+22                char temp = ch[l];
+23                ch[l] = ch[r];
+24                ch[r] = temp;
+25
+26                l++;
+27                r--;
+28            }
+29        }
+30        String str = new String(ch);
+31        return str;
+32    }
+33}
